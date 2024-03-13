2def1b2eb33cace6d52e76ce648466c8076eda00
Adane <adanemoges6@gmail.com>
2024-03-13 06:12:49 +0300

    Refactor DBClient class and add documentation

--- utils/db.js
+++ utils/db.js
@@ -2,30 +2,47 @@ import process from 'process';
 
 const { MongoClient } = require('mongodb');
 
+/**
+ * Class for DBClient
+ */
 class DBClient {
+  /**
+   * Constructor for DBClient
+   */
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const dbName = process.env.DB_DATABASE || 'files_manager';
-    const url = `mongodb://${host}:${port}`;
+    const url = `mongodb://${host}:${port}/${dbName}`;
     this.db = new MongoClient(url, { useUnifiedTopology: true });
-    this.db.connect((err) => {
-      if (err) console.log(err);
-      else console.log('Connection to DB established');
-    });
+    this.db.connect();
   }
+  /**
+   * Check if MongoDB client is connected to the server
+   * @returns {boolean}
+   */
 
   isAlive() {
     return this.db.isConnected();
   }
+  /**
+   * Get the value of a key in Redis
+   * @param {string} key
+   * @returns {Promise<string>}
+   */
 
   nbUsers() {
     return this.db.collection('users').countDocuments();
   }
+  /**
+   * Get the value of a key in Redis
+   * @param {string} key
+   * @returns {Promise<string>}
+   */
 
   nbFiles() {
     return this.db.collection('files').countDocuments();
   }
 }
-const dbClient = new DBClient();
+export const dbClient = new DBClient();
 export default dbClient;
